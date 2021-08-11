5c558833c3a22f237aac2052c4a01cc8a1a9fc73
Cesar <cesaring@hotmail.com>
2021-08-11 01:00:03 -0500

    Have a working index page...

--- src/main/java/com/mambodata/studentDemo/StudentDemoApplication.java
+++ src/main/java/com/mambodata/studentDemo/StudentDemoApplication.java
@@ -2,12 +2,22 @@ package com.mambodata.studentDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return String.format("This is the index...");
 	}
 
 }
